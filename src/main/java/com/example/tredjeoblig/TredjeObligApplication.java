f77b08b1689af237e8e57b2431f0e4c30f8830f3
ellinoratveitholen <s376866@oslomet.no>
2023-04-21 10:57:43 +0200

    Initial commit

---
+++ src/main/java/com/example/tredjeoblig/TredjeObligApplication.java
@@ -0,0 +1,13 @@
+package com.example.tredjeoblig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TredjeObligApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TredjeObligApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tredjeoblig/TredjeObligApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tredjeoblig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TredjeObligApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
